35aed1774a3adc946cb7c814c1149948e0b966c2
Sahidur Rahman Suman <suman5040@gmail.com>
2015-10-22 12:56:45 +0600

    first commit

---
+++ lib/pretty_photo_portfolio.rb
@@ -0,0 +1,5 @@
+require "pretty_photo_portfolio/version"
+
+module PrettyPhotoPortfolio
+  # Your code goes here...
+end
---
+++ spec/pretty_photo_portfolio_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe PrettyPhotoPortfolio do
+  it 'has a version number' do
+    expect(PrettyPhotoPortfolio::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
